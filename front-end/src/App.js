a9dbf2aad6bbdb9dde613506bbd2d84a3b1594b2
Yasin Shuman <email@yasin.io>
2018-08-13 12:35:07 -0700

    created billing component still non-functional

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,6 +1,7 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Billing from "./Components/Billing/Billing";
 
 class App extends Component {
   render() {
--- front-end/src/Components/Billing/Billing.js
+++ front-end/src/Components/Billing/Billing.js
@@ -1,14 +1,46 @@
-import React from 'react';
+import React from "react";
+import "./Billing.css";
+
 export default class Billing extends React.Component {
-  static propTypes = {
-    name: React.PropTypes.string,
-  };
-  constructor(props) {
-    super(props);
-  }
+  // static propTypes = {
+  //   name: React.PropTypes.string
+  // };
+  // constructor(props) {
+  //   super(props);
+  //   state = {
+
+  //   }
+  // }
   render() {
     return (
-      <div></div>
+      <div>
+        <form>
+          <fieldset>
+            <legend>Choose Your Subscription</legend>
+            <div>
+              <input
+                name="subscriptionType"
+                value="monthly"
+                id="monthly"
+                type="checkbox"
+              />
+              <label for="monthly">1 Year Subscription - $9.99</label>
+            </div>
+            <div>
+              <input
+                name="subscriptionType"
+                value="annual"
+                id="annual"
+                type="checkbox"
+              />
+              <label for="annual">1 Year Premium Subscription - $29.99</label>
+            </div>
+          </fieldset>
+          <p>
+            <button class="styled">Buy Now</button>
+          </p>
+        </form>
+      </div>
     );
   }
 }
