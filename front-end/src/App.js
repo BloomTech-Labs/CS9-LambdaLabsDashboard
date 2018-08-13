be638d894e23466c41848fcf82c7c1bc1d88c689
Yasin Shuman <email@yasin.io>
2018-08-13 15:55:31 -0700

    fixed file cases

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
-//import Settings from "./Components/Settings/Settings";
-import Billing from "./Components/Billing/Billing";
+//import Settings from "./Components/Settings/settings";
+import Billing from "./Components/Billing/billing";
 
 class App extends Component {
   render() {
---
+++ front-end/src/Components/Settings/settings.js
@@ -0,0 +1,62 @@
+import React from "react";
+import "./Settings.css";
+//import Sidenav from "../Sidenav/sidenav";
+
+export default class Billing extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      oldPw: "",
+      newPw: ""
+    };
+  }
+
+  onChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  };
+
+  onSubmit = event => {
+    event.preventDefault();
+  };
+
+  render() {
+    return (
+      <div>
+        {/* <Sidenav /> */}
+        <form className="settings">
+          <br />
+          <label>Email:</label>{" "}
+          <input
+            type="text"
+            name="email"
+            value={this.state.email}
+            onChange={this.onChange}
+          />
+          <br />
+          <label>Old Password:</label>{" "}
+          <input
+            type="password"
+            name="oldPw"
+            value={this.state.oldPw}
+            onChange={this.onChange}
+          />
+          <br />
+          <label>New password:</label>{" "}
+          <input
+            type="password"
+            name="newPw"
+            value={this.state.newPw}
+            onChange={this.onChange}
+          />
+          <br />
+          <p>
+            <button className="styled">Save</button>
+          </p>
+        </form>
+      </div>
+    );
+  }
+}
