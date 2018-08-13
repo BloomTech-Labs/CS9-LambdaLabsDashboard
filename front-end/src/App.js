b840913eb52ead563da4f2854998651b988c83be
alexfigliolia <alexfigliolia@gmail.com>
2018-08-13 14:20:07 -0400

    Set up Sass in our project. Imported a CSS reset as well as Sass variables for colors, media queries, and utilities

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,18 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Dashboard from './Components/Dashboard/Dashboard';
 import './App.css';
 
 class App extends Component {
-  render() {
+  render = () => {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {/*Comment my componenet out if you're working on your own feature 
+        at the base level! - Love Alex */}
+        <Dashboard />
       </div>
     );
   }
