1dd7b80538c7598cc6be03b3bee43dcb32d6c294
Boomer <boomer@lambdaschool.com>
2018-08-16 13:52:55 -0700

    Revert "LandingPage - Created Username, UserPassword with Login & Signout Buttons, Created Username & Password for new account"

--- server/server.js
+++ server/server.js
@@ -1,43 +1,7 @@
-import express from "express";
-import mongoose from "mongoose";
+import express from 'express';
 
-const keys = require("./keys.js");
 const Server = express();
 
-const bodyParser = require("body-parser");
-const helmet = require("helmet");
-const cors = require("cors");
-Server.use(cors());
-Server.use(helmet());
-Server.use(bodyParser.json());
-
-const port = process.env.PORT || 4000;
-
-mongoose
-  .connect(
-    keys.mongodb.dbURL,
-    { useNewUrlParser: true }
-  )
-  .then(p => {
-    console.log("=== connected to lambdadashboard==");
-  })
-  .catch(err => {
-    console.log(`err:${err}`);
-  });
-
-Server.get("/", (req, res) => {
-  res.status(200).json({ msg: "api is running!" });
-});
-
-const projects = require("./projects/projectsRoute.js");
-Server.use("/projects", projects);
-
-const user = require("./Users/userRoute.js");
-Server.use("/users", user);
-
-const login = require("./login/loginRoute.js");
-Server.use("/login", login);
-
-Server.listen(port, () => {
-  console.log(`\n=== server is running on ${port} ==`);
+Server.listen(5000, () => {
+	console.log('Server listening on port 5000')
 });
