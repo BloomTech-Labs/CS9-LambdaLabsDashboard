2b500e7cdb5ad43d9c1ba57cfc0bb68c076ba726
MINE <Mine@MINEs-MacBook-Pro-2.local>
2018-08-16 14:49:01 -0700

    Working on CSS

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import "./App.css";
 import { Route } from "react-router-dom";
 import Classes from "./Components/Classes/classes.js";
-import LandingPage from "./Components/LandingPage/LandingPage.js";
+import LandingPage from "./Components/LandingPage/landingPage.js";
 import Projects from "./Components/Projects/projects.js";
 import Billing from "./Components/Billing/billing.js";
 // import Settings from './Components/Settings/settings.js'
@@ -12,9 +12,9 @@ class App extends Component {
     return (
       <div>
         <Route exact path="/" component={LandingPage} />
-        <Route path="/classes" component={Classes} />
-        <Route path="/projects" component={Projects} />
-        <Route path="/Billing" component={Billing} />
+        <Route path="app/classes" component={Classes} />
+        <Route path="app/projects" component={Projects} />
+        <Route path="app/billing" component={Billing} />
       </div>
     );
   }
