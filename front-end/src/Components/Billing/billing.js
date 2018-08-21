296d885271f8e903e9e33b9c2d7424e6f7edf029
hilal aissani <hilalaissani@gmail.com>
2018-08-20 20:07:29 -0400

    server is working

--- front-end/src/Components/Billing/billing.js
+++ front-end/src/Components/Billing/billing.js
@@ -2,10 +2,6 @@ import React from "react";
 import axios from "axios";
 import StripeCheckout from "react-stripe-checkout";
 import Stripe from "./stripe";
-<<<<<<< HEAD
-=======
-const stripe = require("stripe-client")("pk_test_dtZeEKgd6FSjpH2sFi8RAYFa");
->>>>>>> 646ac2750864df380e774b8b3c82efb1dfe860b3
 
 export default class Billing extends React.Component {
   constructor(props) {
