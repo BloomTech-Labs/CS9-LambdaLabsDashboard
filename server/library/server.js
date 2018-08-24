d6c2c8b48a7dc9b0eaf010e29b945aaf26660987
alexfigliolia <alexfigliolia@gmail.com>
2018-08-23 21:02:38 -0400

    update build folders

--- server/library/server.js
+++ server/library/server.js
@@ -29,6 +29,14 @@ const staticFiles = express.static(
 Server.use(cors());
 Server.use(helmet());
 Server.use(bodyParser.json());
+Server.use((req, res, next) => {
+ res.setHeader('Access-Control-Allow-Origin', '*');
+ res.setHeader('Access-Control-Allow-Credentials', 'true');
+ res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE');
+ res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
+ res.setHeader('Cache-Control', 'no-cache');
+ next();
+});
 Server.use(staticFiles);
 
 const port = process.env.PORT || 4000;
