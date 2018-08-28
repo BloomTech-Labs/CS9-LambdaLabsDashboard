550449574796869f4075621f9c904f287409fe75
hilal aissani <hilalaissani@gmail.com>
2018-08-28 12:06:34 -0400

    lastet version

--- front-end/src/Actions/ExternalApis.js
+++ front-end/src/Actions/ExternalApis.js
@@ -1,9 +1,10 @@
-<<<<<<< HEAD
 import Axios from "axios";
+const baseURL =
+  process.env.NODE_ENV === "production" ? "" : "http://localhost:4000";
 
 export const getDataForProject = repository => {
   return dispatch => {
-    Axios.post("/externalApis", { repository })
+    Axios.post(`${baseURL}/externalApis`, { repository })
       .then(api => {
         if (api.data) {
           const {
@@ -25,26 +26,6 @@ export const getDataForProject = repository => {
       .catch(err => dispatch(handleError(err)));
   };
 };
-=======
-import Axios from 'axios';
-const baseURL = process.env.NODE_ENV === 'production' ? '' : 'http://localhost:4000';
-
-export const getDataForProject = repository => {
-	return dispatch => {
-		Axios.post(`${baseURL}/externalApis`, { repository })
-    .then(api => {
-      if(api.data) {
-        const { project, team, trello, totalCards, inProgress, completeness } = api.data;
-        dispatch(initializeProjectData(project, team, trello, totalCards, inProgress));
-        setTimeout(() => dispatch(animateDashboard(completeness)), 250);
-      } else {
-        dispatch(handleError(true))
-      }
-    })
-    .catch(err => dispatch(handleError(err)));
-	}
-}
->>>>>>> bf5b460eecad7fb0a9442164fb900fa3b6cd4c1a
 
 const handleError = error => {
   return { type: "ERROR", error };
