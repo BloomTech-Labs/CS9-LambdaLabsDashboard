bf2b8a942c25e73d1f5821db837382407b7346a9
JacquelynnRohrich <33401573+JacquelynnRohrich@users.noreply.github.com>
2018-09-06 15:19:01 -0500

    fixed navbar

--- front-end/src/Components/Main/Main.js
+++ front-end/src/Components/Main/Main.js
@@ -8,7 +8,7 @@ class Main extends Component {
             <div className="main-container">
                 <header>
                     <nav class="navbar navbar-expand-lg navbar-dark mdb-color darken-3 fixed-top">
-                        <a class="navbar-brand" href="#"><img src="../src/pictures/logo.png" height="30" alt=""></img></a>
+                        <a class="navbar-brand" href="#"><img src="./favicon/fav/logo.png" height="30" alt=""></img></a>
                         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#basicExampleNav" aria-controls="basicExampleNav"
                             aria-expanded="false" aria-label="Toggle navigation">
                             <span class="navbar-toggler-icon"></span>
@@ -26,37 +26,43 @@ class Main extends Component {
                                 <li class="nav-item">
                                     <a class="nav-link" href="#">Pricing</a>
                                 </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">Team</a>
+                                </li>
                             </ul>
-                            {/* <form class="form-inline">
+                            <form class="form-inline my-1">
+                                   <a href="/login"class="btn btn-outline-black btn-sm my-0" type="submit">Sign In</a>
+                            </form>
+                                {/* <form class="form-inline">
                                 <div class="md-form mt-0">
                                     <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search"></input>
                                 </div>
                             </form> */}
                         </div>
                     </nav>
-                    <div id="intro" class="view">
-                        <div class="mask">
+                        <div id="intro" class="view">
+                            <div class="mask">
+                            </div>
                         </div>
-                    </div>
                 </header>
-                <div class="container">
-                    <div class="row">
-                        <div class="col-md-7">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-md-7">
+                            </div>
+                            <div class="col-md-5">
+                            </div>
                         </div>
-                        <div class="col-md-5">
+                        <div class="row">
+                            <div class="col-lg-4 col-md-12">
+                            </div>
+                            <div class="col-lg-4 col-md-6">
+                            </div>
+                            <div class="col-lg-4 col-md-6">
+                            </div>
                         </div>
                     </div>
-                    <div class="row">
-                        <div class="col-lg-4 col-md-12">
-                        </div>
-                        <div class="col-lg-4 col-md-6">
-                        </div>
-                        <div class="col-lg-4 col-md-6">
-                        </div>
-                    </div>
-                </div>
             </div>
-        );
-    }
-}
+                );
+            }
+        }
 export default Main;
